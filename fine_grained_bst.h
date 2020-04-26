26e8c413716988061d30f33969b62440ee488e75
Angelos Spiliotis <45153901+aggelos-sp@users.noreply.github.com>
2020-04-26 17:12:23 +0300

    Create fine_grained_bst.h

---
+++ fine_grained_bst.h
@@ -0,0 +1,15 @@
+#include <pthread.h>
+#include <stdlib.h>
+
+typedef struct bst_node {
+  int songID; 
+  struct bst_node *lc;
+  struct bst_node *rc;
+  pthread_mutex_t lock;
+}T_NODE;
+
+
+T_NODE* create_node(int songID);
+T_NODE* search(int songID, T_NODE* root);
+int remove(int songID, T_NODE* root);
+void instert(int songID, T_NODE* root, T_NODE* parent);
