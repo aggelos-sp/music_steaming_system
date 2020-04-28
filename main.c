3bb5c526ec1b3316c5fe32903f6f44b8c6a66904
aggelos-sp <45153901+aggelos-sp@users.noreply.github.com>
2020-04-28 15:55:34 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include "fine_grained_bst.h"
-
+#include "unbounded_queue.h"
 
 #define ANSI_COLOR_RED     "\x1b[31m"
 #define ANSI_COLOR_GREEN     "\x1b[32m"
@@ -8,7 +8,8 @@
 int number_of_threads = 0;
 int bst_node_number = 0;
 int bst_checksum = 0;
-pthread_barrier_t first_b;
+pthread_barrier_t first_barrier;
+pthread_barrier_t second_barrier;
 
 void* generate_songs(void *id){
     printf("I am in\n");
@@ -19,7 +20,7 @@ void* generate_songs(void *id){
         printf("I am thread: %d and going to insert:%d\n",(int)id,song_id);
         insert(song_id,global_root,NULL);
     }
-    pthread_barrier_wait(&first_b);
+    pthread_barrier_wait(&first_barrier);
 }
 void tree_counter(T_NODE* node) 
 { 
@@ -48,20 +49,22 @@ void first_check(void * arg){
     }
     
 }
+
 int main(int argc, char *argv[]){
-    
+    int i = 0;
     if(argc != 2){
         printf("Usage: a.out <thread count>\n");
         exit(0);
     }
-    int i = 0;
-    printf("Init tree start\n");
     init_tree();
-    printf("Init tree end\n");
     number_of_threads = atoi(argv[1]);
     printf("Number of threads %d\n",number_of_threads);
+    init_queues(number_of_threads/2);
     pthread_t my_threads[number_of_threads];
-    pthread_barrier_init(&first_b, NULL, number_of_threads);
+    /*Init barriers*/
+    pthread_barrier_init(&first_barrier, NULL, number_of_threads);
+    pthread_barrier_init(&second_barrier, NULL, number_of_threads);
+    /*Start thread for tree insertion*/
     for(i = 0; i < number_of_threads; i++){
         pthread_create(&my_threads[i], NULL,(void*)generate_songs,(void*)i);
     }
@@ -69,6 +72,7 @@ int main(int argc, char *argv[]){
         pthread_join(my_threads[i],NULL);
     }
     first_check(NULL);
-    pthread_barrier_destroy(&first_b);
+    pthread_barrier_destroy(&first_barrier);
+    pthread_attr_destroy(&second_barrier);
     return 0;
 }
