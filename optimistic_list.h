681a5eab41d2a9cac52cfd1287ff86d7600260dc
aggelos-sp <45153901+aggelos-sp@users.noreply.github.com>
2020-04-29 21:39:22 +0300

    Create optimistic_list.h

---
+++ optimistic_list.h
@@ -0,0 +1,17 @@
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+#define true 1
+#define false 0
+typedef struct list_node {
+  int songID; 
+  struct list_node *next;
+  pthread_mutex_t lock;
+}L_NODE;
+
+L_NODE* create_list_node(int songID);
+int validate(L_NODE* pred, L_NODE* curr);
+int insert_list(int songID);
+
