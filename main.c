cf5758f19fa26e23172c82c7d5fa38a66c3884c6
aggelos-sp <45153901+aggelos-sp@users.noreply.github.com>
2020-04-29 20:05:03 +0300

    Update main.c

--- main.c
+++ main.c
@@ -61,6 +61,7 @@ void* second(void* arg){
         printf("I am thread %d and starting to search (%d)..\n",id, i);
         result = delete_util(i, global_root);
         pthread_mutex_unlock(&result->lock);
+        pthread_mutex_unlock(&result->p->lock);
         printf("I am thread %d and stopped search\n",id);
         if(result != 0){
             printf(ANSI_COLOR_GREEN"Node with id = (%d) found.\n"ANSI_COLOR_RESET, result->songID);
