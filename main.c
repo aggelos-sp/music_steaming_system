b0f3ffccaf508086a022b33bcf975dd2fac42a0c
aggelos-sp <45153901+aggelos-sp@users.noreply.github.com>
2020-04-29 18:55:31 +0300

    Update main.c

--- main.c
+++ main.c
@@ -57,8 +57,7 @@ void* second(void* arg){
     int id = (int) arg;
     int i = 0;
     int result = -1;
-    for(i = 0; i <= 3; i++){
-    //for(i = (N*id); i <= ((N*id) + (N - 1)); i++){
+    for(i = (N*id); i <= ((N*id) + (N - 1)); i++){
         printf("I am thread %d and starting search\n",id);
         result = search(i, global_root, NULL);
         printf("I am thread %d and stopped search",id);
